5bf39adb79b776156272d14f0bc35ea7a7547ba6
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-12-14 08:19:47 -0300

    first commit

---
+++ src/maps/places/places.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Query } from '@nestjs/common';
+
+@Controller('places')
+export class PlacesController {
+
+  @Get()
+  findPlaces(@Query('text') text: string) {
+    return `Meus endereço e ${text}`
+  }
+
+}
